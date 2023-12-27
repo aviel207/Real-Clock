744c646cc55a4d111fe17189da23a7712950ef2e
aviel207 <137002946+aviel207@users.noreply.github.com>
2023-12-27 10:40:07 +0200

    Update tb_top_clk_v4.v

--- simulation/tb_tops/tb_top_clk_v4.v
+++ simulation/tb_tops/tb_top_clk_v4.v
@@ -4,6 +4,9 @@ module tb_top_clk_v4();
 	
 	reg  		tb_clk;
 	reg  		tb_reset; 
+	reg 		tb_load;
+	reg  [1:0]	tb_addrs;
+	reg  [5:0]	tb_data_in;
 	wire [5:0]  tb_seconds_out;
 	wire [5:0]	tb_minutes_out;	
  	wire [4:0]	tb_hours_out;
@@ -12,8 +15,14 @@ module tb_top_clk_v4();
 	begin
 		tb_clk = 1'b0;
 		tb_reset = 1'b1;
+		tb_load = 1'b0;
+		tb_addr = 2'b10;
+		tb_data_in = 6'd17;
 		#100 
 		tb_reset = 1'b0;
+		#500
+		tb_load = 1'b1;
+		tb_load = 1'b0;
 	end
  
 	always
@@ -25,6 +34,9 @@ module tb_top_clk_v4();
 	(
 		tb_clk  ,
 		tb_reset,
+		tb_load,
+		tb_addrs,
+		tb_data_in,
 		tb_seconds_out,
 		tb_minutes_out,
 		tb_hours_out
