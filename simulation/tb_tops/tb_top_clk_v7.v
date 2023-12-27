9a6ed54c17465e368df80b7a81d6d79c8f2cd29c
aviel207 <137002946+aviel207@users.noreply.github.com>
2023-12-27 10:46:46 +0200

    Update tb_top_clk_v7.v

--- simulation/tb_tops/tb_top_clk_v7.v
+++ simulation/tb_tops/tb_top_clk_v7.v
@@ -4,17 +4,26 @@ module tb_top_clk_v7();
 	
 	reg  		tb_clk;
 	reg  		tb_reset; 
-	reg			tb_mode;
+	reg		tb_mode;
+	reg 		tb_load;
+	reg  [1:0]	tb_addrs;
+	reg  [5:0]	tb_data_in;
 	wire [3:0]	tb_basys_anode;
 	wire [6:0]	tb_display_ssd;
 
 	initial 
 	begin
-		tb_clk   = 1'b0;
+		tb_clk = 1'b0;
 		tb_reset = 1'b1;
-		tb_mode  = 1'b0;
+		tb_load = 1'b0;
+		tb_addr = 2'b00;
+		tb_data_in = 6'd40;
 		#100 
 		tb_reset = 1'b0;
+		#500
+		tb_load = 1'b1;
+		tb_load = 1'b0;
+		tb_mode  = 1'b0;
 		#500
 		tb_mode  = 1'b1;
 	end
@@ -29,6 +38,9 @@ module tb_top_clk_v7();
 		tb_clk,
 		tb_reset,
 		tb_mode,
+		tb_load,
+		tb_addrs,
+		tb_data_in,
 		tb_basys_anode,
 		tb_display_ssd
 	);
