2eee4a21006c9559bb46f66dc788ccdcb0025456
aviel207 <137002946+aviel207@users.noreply.github.com>
2023-12-27 10:49:34 +0200

    Update top_clk_v2.v

--- tops/top_clk_v2.v
+++ tops/top_clk_v2.v
@@ -4,7 +4,10 @@
 module top_clk_v2
 (
 	input  wire 		clk,    		
-	input  wire 		reset,  		
+	input  wire 		reset, 
+	input  wire		load,
+	input  wire [1:0]	addrs,
+	input  wire [5:0]	data_in,
 	output wire [5:0]	seconds_out   	
 );
 	wire tc_time_base_wire;
@@ -21,6 +24,9 @@ module top_clk_v2
 		.clk(clk),
 		.reset(reset),
 		.tc_time_base(tc_time_base_wire),
+		.load(load),
+		.addrs(addrs),
+		.data_in(data_in),
 		.q_seconds(seconds_out)
 	);
 
